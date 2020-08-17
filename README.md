9e3a1a5ca5c1996bc329d0597333a234baf70d3f
Jamie Wood <me@jamiewood.io>
2020-08-17 12:23:32 +0100

    serverless deploy

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
-cloudflare-ip-security-group-update
-===================================
+# Cloudflare's IP address list > AWS Security Group and S3 Policies
 
 This Lambda function to retrieve Cloudflare's IP address list and
 update an AWS Security Group and S3 Policies.
 
-It was originally written by John McCracken (johnmccuk@gmail.com), 
+It was originally written by John McCracken (johnmccuk@gmail.com),  
 updated by Ryan Gibbons (rtgibbons) and Endrigo Antonini (antonini).
 
-Instructions
-------------
+We have made a fork that we are expanding and optimizing for our use cases and hopefully for more generic use cases. It could at some point be a version two written in go. Hans Øyvind Laderud (afreakk) and Christian Sakshaug (csakshaug) supporting this fork on behalf of dx.
+
+## Instructions
+
+### Serverless
+
+Serverless will automatically create and manage a security group in the specified vpc.
+And add cloudflare ips to its inbound rules on ports specified.
+(S3 permissions is not set up in serverless.yml because i dont need it, but you can uncomment the permissions in serverless.yml)
+
+```bash
+serverless deploy  --vpcid "vpc-12345678912345678" --ports "80,443"
+```
+
+### Manual
 
 Use the content of the file `cf-security-group-update.py` in your lambda ou upload it.
 
@@ -17,13 +29,10 @@ It is also required that you upload or create the `package` file as is available
 The Lambda uses the Python 2.7 runtime and requires the following
 enviroment variables:
 
-* `SECURITY_GROUP_IDS_LIST` - a list of security group IDs to update
-* `SECURITY_GROUP_ID` - If list is undefined, a group ID for the specified security group
-* `PORTS_LIST` - comma-separated list of ports e.g. `80,443`.
+* `SECURITY_GROUP_IDS_LIST` - security group ID(s) to update `sg-12345678912345678,s3-11122223333344444`
+* `PORTS_LIST` - port(s) to update `80,443` or `80` (defaults to 80 if none specified)
 * `S3_CLOUDFLARE_SID` - Sid that stores all the CloudFlare configurataion. That Sid is stored on the Stament policy.
-* `S3_BUCKET_IDS_LIST` - a list of S3 buckets IDs to update
-* `S3_BUCKET_ID` - if list is undefined, a ID for the specified S3 bucket.
-* `UPDATE_IPV6` - if set to 0, will not update IPv6 ranges in security groups nor S3 bucket policies.
+* `S3_BUCKET_IDS_LIST` - S3 buckets ID(s) to update
 
 You need to allow the Lambda to execute those actions (example on the file `allow-lambda-ingress-role`:
 
@@ -34,24 +43,19 @@ You need to allow the Lambda to execute those actions (example on the file `allo
 * s3:PutBucketPolicy
 * s3:GetBucketPolicy
 
+## To update Security Groups
 
-
-To update Security Groups
-------------
-
-You need to define at least `SECURITY_GROUP_ID` or `SECURITY_GROUP_IDS_LIST`.
+You need to define `SECURITY_GROUP_IDS_LIST`.
 The parameter `PORTS_LIST` is also used to update an AWS Security Group.
 
-To update S3 Policy
-------------
+## To update S3 Policy
 
-You need to define the parameter `S3_CLOUDFLARE_SID` and at least one of the
-following parameters `S3_BUCKET_IDS_LIST` or `S3_BUCKET_ID`.
+You need to define the parameters `S3_CLOUDFLARE_SID` and `S3_BUCKET_IDS_LIST`.
 
-Contributors
------------
+## Contributors
 
 * John McCracken ([@johnmccuk](https://www.github.com/johnmccuk))
 * Ryan Gibbons ([@rtgibbons](https://www.github.com/rtgibbons)) 
 * Ben Steinberg ([@bensteinberg](https://www.github.com/bensteinberg))
 * Endrigo Antonini ([@antonini](https://www.github.com/antonini))
+* Jamie Wood ([@woodjme](https://www.github.com/woodjme))
--- cf-security-group-update.py
+++ cf-security-group-update.py
@@ -13,6 +13,7 @@ def get_cloudflare_ip_list():
         return temp['result']
     raise Exception("Cloudflare response error")
 
+
 def get_aws_s3_bucket_policy(s3_id):
     """ Return the Policy of an S3 """
     s3 = boto3.client('s3')
@@ -20,7 +21,7 @@ def get_aws_s3_bucket_policy(s3_id):
     if not 'Policy' in result:
         raise Exception("Failed to retrieve Policy from S3 %s" % (s3_id))
     policy = json.loads(result['Policy'])
-    return { 'id' : s3_id, s3_id : policy }
+    return {'id': s3_id, s3_id: policy}
 
 
 def get_aws_security_group(group_id):
@@ -48,7 +49,8 @@ def add_ipv4_rule(group, address, port):
                             FromPort=port,
                             ToPort=port,
                             GroupName=group.group_name)
-    print("Added %s : %i to %s (%s) " % (address, port, group.group_id, group.group_name))
+    print("Added %s : %i to %s (%s) " %
+          (address, port, group.group_id, group.group_name))
 
 
 def delete_ipv4_rule(group, address, port):
@@ -57,7 +59,8 @@ def delete_ipv4_rule(group, address, port):
                          CidrIp=address,
                          FromPort=port,
                          ToPort=port)
-    print("Removed %s : %i from %s (%s) " % (address, port, group.group_id, group.group_name))
+    print("Removed %s : %i from %s (%s) " %
+          (address, port, group.group_id, group.group_name))
 
 
 def check_ipv6_rule_exists(rules, address, port):
@@ -71,17 +74,22 @@ def check_ipv6_rule_exists(rules, address, port):
 
 def add_ipv6_rule(group, address, port):
     """ Add the IP address/port to the security group """
-    group.authorize_ingress(GroupName=group.group_name, IpPermissions=[{
-        'IpProtocol': "tcp",
-        'FromPort': port,
-        'ToPort': port,
-        'Ipv6Ranges': [
-            {
-                'CidrIpv6': address
-            },
-        ]
-    }])
-    print("Added %s : %i to %s (%s) " % (address, port, group.group_id, group.group_name))
+    group.authorize_ingress(GroupName=group.group_name,
+                            IpPermissions=[{
+                                'IpProtocol':
+                                "tcp",
+                                'FromPort':
+                                port,
+                                'ToPort':
+                                port,
+                                'Ipv6Ranges': [
+                                    {
+                                        'CidrIpv6': address
+                                    },
+                                ]
+                            }])
+    print("Added %s : %i to %s (%s) " %
+          (address, port, group.group_id, group.group_name))
 
 
 def delete_ipv6_rule(group, address, port):
@@ -96,13 +104,16 @@ def delete_ipv6_rule(group, address, port):
             },
         ]
     }])
-    print("Removed %s : %i from %s (%s) " % (address, port, group.group_id, group.group_name))
+    print("Removed %s : %i from %s (%s) " %
+          (address, port, group.group_id, group.group_name))
+
 
 def get_update_ipv6():
     try:
         return bool(int(os.environ['UPDATE_IPV6']))
     except (KeyError, ValueError):
         return True
+
 
 def update_s3_policies_policies(ip_addresses):
     """ Update S3 policies """
@@ -111,11 +122,15 @@ def update_s3_policies_policies(ip_addresses):
     s3 = boto3.client('s3')
 
     if not "S3_CLOUDFLARE_SID" in os.environ:
-        print("Not configured 'S3_CLOUDFLARE_SID' variable, so will not check S3")
+        print(
+            "Not configured 'S3_CLOUDFLARE_SID' variable, so will not check S3"
+        )
         return
 
     if not "S3_BUCKET_IDS_LIST" in os.environ and not "S3_BUCKET_ID" in os.environ:
-        raise Exception("Missing S3 basic configuration 'S3_BUCKET_IDS_LIST' or 'S3_BUCKET_ID'.") 
+        raise Exception(
+            "Missing S3 basic configuration 'S3_BUCKET_IDS_LIST' or 'S3_BUCKET_ID'."
+        )
 
     ipv4 = ip_addresses['ipv4_cidrs']
     ipv6 = ip_addresses['ipv6_cidrs']
@@ -127,33 +142,40 @@ def update_s3_policies_policies(ip_addresses):
         cloudflare_ips = ipv4
 
     sid = os.environ['S3_CLOUDFLARE_SID']
-    s3_policy_tuple = map(get_aws_s3_bucket_policy, os.environ['S3_BUCKET_IDS_LIST'].split(","))
+    s3_policy_tuple = map(get_aws_s3_bucket_policy,
+                          os.environ['S3_BUCKET_IDS_LIST'].split(","))
     if not s3_policy_tuple:
-        s3_policy_tuple = [get_aws_s3_bucket_policy(os.environ['S3_BUCKET_ID'])]
+        s3_policy_tuple = [
+            get_aws_s3_bucket_policy(os.environ['S3_BUCKET_ID'])
+        ]
 
     for s3_tuple in s3_policy_tuple:
         updated = False
         s3_id = s3_tuple['id']
-        print("Checking Policy of S3 Bucket '%s'" % (s3_id) )
+        print("Checking Policy of S3 Bucket '%s'" % (s3_id))
         policy = s3_tuple[s3_id]
         if not 'Statement' in policy:
-            raise Exception("Problem reading policy of S3 Bucket '%s'" % (s3_id) )
+            raise Exception("Problem reading policy of S3 Bucket '%s'" %
+                            (s3_id))
         for statement in policy['Statement']:
             if not "Sid" in statement:
-                raise Exception("Problem reading Sid inside Statement of S3 Bucket '%s'" % (s3_id) )
-            if ((not sid == statement['Sid']) or
-              (not "Condition" in statement) or
-              (not "IpAddress" in statement["Condition"]) or
-              (not "aws:SourceIp" in statement["Condition"]["IpAddress"])):
+                raise Exception(
+                    "Problem reading Sid inside Statement of S3 Bucket '%s'" %
+                    (s3_id))
+            if ((not sid == statement['Sid']) or (not "Condition" in statement)
+                    or (not "IpAddress" in statement["Condition"]) or
+                (not "aws:SourceIp" in statement["Condition"]["IpAddress"])):
                 continue
 
-            statement["Condition"]["IpAddress"]["aws:SourceIp"] = cloudflare_ips
+            statement["Condition"]["IpAddress"][
+                "aws:SourceIp"] = cloudflare_ips
             updated = True
 
         if updated:
             policy = json.dumps(policy)
-            print("Going to update policy %s " % (s3_id) )
+            print("Going to update policy %s " % (s3_id))
             s3.put_bucket_policy(Bucket=s3_id, Policy=policy)
+
 
 def update_security_group_policies(ip_addresses):
     """ Update Information of Security Groups """
@@ -165,7 +187,9 @@ def update_security_group_policies(ip_addresses):
         try:
             security_groups = os.environ['SECURITY_GROUP_ID']
         except KeyError:
-            print('Missing environment variables SECURITY_GROUP_IDS_LIST and SECURITY_GROUP_ID. Will not update security groups.')
+            print(
+                'Missing environment variables SECURITY_GROUP_IDS_LIST and SECURITY_GROUP_ID. Will not update security groups.'
+            )
             return
 
     security_groups = map(get_aws_security_group, security_groups.split(','))
@@ -178,10 +202,11 @@ def update_security_group_policies(ip_addresses):
     ports = map(int, ports.split(','))
 
     if (not ports) or (not security_groups):
-        raise Exception('At least one TCP port and one security group ID are required.')
+        raise Exception(
+            'At least one TCP port and one security group ID are required.')
 
     ## Security Groups
-    for security_group in security_groups: 
+    for security_group in security_groups:
         current_rules = security_group.ip_permissions
         ## IPv4
         # add new addresses
@@ -189,37 +214,45 @@ def update_security_group_policies(ip_addresses):
             for port in ports:
                 if not check_ipv4_rule_exists(current_rules, ipv4_cidr, port):
                     add_ipv4_rule(security_group, ipv4_cidr, port)
-    
+
         # remove old addresses
         for port in ports:
             for rule in current_rules:
                 # is it necessary/correct to check both From and To?
-                if rule['IpProtocol'] == 'tcp' and rule['FromPort'] == port and rule['ToPort'] == port:
+                if rule['IpProtocol'] == 'tcp' and rule[
+                        'FromPort'] == port and rule['ToPort'] == port:
                     for ip_range in rule['IpRanges']:
-                        if ip_range['CidrIp'] not in ip_addresses['ipv4_cidrs']:
-                            delete_ipv4_rule(security_group, ip_range['CidrIp'], port)
-    
+                        if ip_range['CidrIp'] not in ip_addresses[
+                                'ipv4_cidrs']:
+                            delete_ipv4_rule(security_group,
+                                             ip_range['CidrIp'], port)
+
         ## IPv6 -- because of boto3 syntax, this has to be separate
         if get_update_ipv6():
             # add new addresses
             for ipv6_cidr in ip_addresses['ipv6_cidrs']:
                 for port in ports:
-                    if not check_ipv6_rule_exists(current_rules, ipv6_cidr, port):
+                    if not check_ipv6_rule_exists(current_rules, ipv6_cidr,
+                                                  port):
                         add_ipv6_rule(security_group, ipv6_cidr, port)
 
             # remove old addresses
             for port in ports:
                 for rule in current_rules:
-                    if rule['IpProtocol'] == 'tcp' and rule['FromPort'] == port and rule['ToPort'] == port:
+                    if rule['IpProtocol'] == 'tcp' and rule[
+                            'FromPort'] == port and rule['ToPort'] == port:
                         for ip_range in rule['Ipv6Ranges']:
-                            if ip_range['CidrIpv6'] not in ip_addresses['ipv6_cidrs']:
-                                delete_ipv6_rule(security_group, ip_range['CidrIpv6'], port)
+                            if ip_range['CidrIpv6'] not in ip_addresses[
+                                    'ipv6_cidrs']:
+                                delete_ipv6_rule(security_group,
+                                                 ip_range['CidrIpv6'], port)
         else:
             print('Not updating IPv6 ranges in security groups.')
 
+
 def lambda_handler(event, context):
     """ AWS Lambda main function """
-    
+
     ip_addresses = get_cloudflare_ip_list()
 
     update_security_group_policies(ip_addresses)
